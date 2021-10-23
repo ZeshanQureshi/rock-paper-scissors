eaaca13f142d857a9b73846b3bfd39e1689603b6
Zeshan <zeshan@qureshi.com>
2021-10-22 22:07:55 -0400

    Updated player buttons

--- scripts/main.js
+++ scripts/main.js
@@ -3,45 +3,27 @@ let gameWon = 0;
 let computerWins = 0;
 let playerWins = 0;
 
+let Ro = "Rock";
+let Pa = "Paper";
+let Sc = "Scissors";
+
 function computerPlay() {
-    let computerChoices = ["ROCK", "PAPER", "SCISSORS"];
-    return computerChoices[Math.floor(Math.random()*3)];
+    let paragraphComputer = document.getElementById("computerPick");
+
+    let computerChoices = ["Rock", "Paper", "Scissors"];
+    let computerDecision = computerChoices[Math.floor(Math.random()*3)];
+    
+    paragraphComputer.innerHTML = computerDecision;
+
+    return computerChoices;
 }
 
-function playRound() {
-    let computerMove = computerPlay();
-    let playerInput = window.prompt("Move?");
+function playRound(buttonClick) {
+    let paragraphPlayer = document.getElementById("playerPick");
+    paragraphPlayer.innerHTML = buttonClick;
 
-    let playerMove = playerInput.toUpperCase();
+    let computerRun = computerPlay();
 
-    if (computerMove == playerMove) {
-        console.log("Tied");
-    } else if (computerMove == "ROCK" && playerMove == "SCISSORS" || 
-               computerMove == "PAPER" && playerMove == "ROCK" || 
-               computerMove == "SCISSORS" && playerMove == "PAPER") {
-                   console.log("Computer Wins!");
-                   computerWins = ++computerWins;
-               } else {
-                   console.log("Player Wins!");
-                   playerWins = ++ playerWins;
-               }
-
-    console.log(computerWins);
-    console.log(playerWins);
-}
-
-function game() {
-
-    while (gameWon == 0) {
-        playRound(playerWins, computerWins);
-
-        if (playerWins == 5) {
-            console.log("Player won 5 rounds");
-            gameWon = 1;
-        } else if (computerWins == 5){
-            console.log("Computer won 5 rounds")
-            gameWon = 1;
-        }
-    }
+    
 }
     
