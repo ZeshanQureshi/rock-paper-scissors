9253250d25f1cfd3ba8866729e03b17cba1b3416
Zeshan <zeshan@qureshi.com>
2021-10-22 22:26:24 -0400

    Completed game functions

--- scripts/main.js
+++ scripts/main.js
@@ -15,15 +15,40 @@ function computerPlay() {
     
     paragraphComputer.innerHTML = computerDecision;
 
-    return computerChoices;
+    return computerDecision;
 }
 
 function playRound(buttonClick) {
     let paragraphPlayer = document.getElementById("playerPick");
     paragraphPlayer.innerHTML = buttonClick;
 
+    let paragraphGame = document.getElementById("gameStatus");
+    let scoreGame = document.getElementById("gameScore");
+
     let computerRun = computerPlay();
 
-    
+    if (computerRun == buttonClick) {
+        paragraphGame.innerHTML = "Both players played " + buttonClick + ". It's a Tie!";
+    } else if (buttonClick == "Rock" && computerRun == "Scissors") {
+        paragraphGame.innerHTML = "Rock beats Scissors. You Win!";
+        playerWins++;
+    } else if (buttonClick == "Paper" && computerRun == "Rock") {
+        paragraphGame.innerHTML = "Paper beats Rock. You Win!";
+        playerWins++;
+    } else if (buttonClick == "Scissors" && computerRun == "Paper") {
+        paragraphGame.innerHTML = "Scissors beats Paper. You Win!";
+        playerWins++;
+    } else if (computerRun == "Rock" && buttonClick == "Scissors") {
+        paragraphGame.innerHTML = "Rock beats Scissors. Computer Wins!";
+        computerWins++;
+    } else if (computerRun == "Paper" && buttonClick == "Rock") {
+        paragraphGame.innerHTML = "Paper beats Rock. Computer Wins!";
+        computerWins++;
+    } else {
+        paragraphGame.innerHTML = "Scissors beats Paper. Computer Wins!";
+        computerWins++;
+    }
+
+    scoreGame.innerHTML = "Score: " + playerWins + " - " + computerWins + "";
 }
     
